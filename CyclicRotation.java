15b9b7fc48bf16c0bd1de091e65f987c20684a59
drabkov <sergeydrabkov@gmail.com>
2016-12-27 13:26:55 +0300

    Update and rename  CyclicRotation to CyclicRotation.java

--- CyclicRotation.java
+++ CyclicRotation.java
@@ -1,10 +1,4 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
-
-class Solution {
+class CyclicRotation {
     public int[] solution(int[] A, int K) {
         int newArray[] = new int[A.length];
         for (int i = 0; i < A.length; i++) {
