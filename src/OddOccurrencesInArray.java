f2d09369fe764a252fffede7756fc54d5f1801a7
drabkov <sergeydrabkov@gmail.com>
2016-12-28 21:17:51 +0300

    Update OddOccurrencesInArray.java

--- src/OddOccurrencesInArray.java
+++ src/OddOccurrencesInArray.java
@@ -1,2 +1,10 @@
 class OddOccurrencesInArray {
+    public int solution(int [] A){
+        int i;
+        int res = 0;
+        for (i = 0; i < A.length; i++) {
+            res = res ^ A[i];
+        }
+        return res;
+    }
 }
